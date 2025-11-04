70906b0769130c51a202a71faf80dcd72fc173f3
hiromia006 <hiromia006@gmail.com>
2025-11-04 22:17:22 +0600

    Add grocery order management tests with authorization and comments

--- src/test/java/click/simple_grocery_store_api/GroceryApiTest.java
+++ src/test/java/click/simple_grocery_store_api/GroceryApiTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class GroceryApiTest extends BaseGroceryApiTest {
     String accessToken = "0511a065f23cd1aad05fa0b2b435d3952425b8c9d3cccd46a306d6f911515fcd";
@@ -134,22 +135,49 @@ Create a new order
 
     @Test(priority = 4)
     public void deleteOrderShouldSucceed() {
-        String orderId = getOderId();
-
-
         given()
                 .spec(getRequestSpecification())
-//                .header("Authorization", "Bearer " + registerNewApiClient())
                 .header("Authorization", "Bearer " + accessToken)
                 .log().uri()
                 .when()
-                .delete("/orders/" + orderId)
+                .delete("/orders/" + getOderId())
                 .then()
                 .log().body()
                 .statusCode(204);
+    }
+
+    @Test
+    public void deleteOrderShouldFailForAccessToken() {
+        given()
+                .spec(getRequestSpecification())
+                .header("Authorization", "Bearer sdfdsjfjiodsfsdahfdshfoshdfhd fsdidifhiisdafh" )
+                .log().uri()
+                .when()
+                .delete("/orders/" + getOderId())
+                .then()
+                .log().body()
+                .statusCode(401)
+                .body("error", equalTo("Invalid bearer token."));
 
 
     }
+
+    @Test
+    public void deleteOrderShouldFailForInvalidId() {
+        given()
+                .spec(getRequestSpecification())
+                .header("Authorization", "Bearer "+accessToken )
+                .log().uri()
+                .when()
+                .delete("/orders/sdsdasdasd"  )
+                .then()
+                .log().body()
+                .statusCode(404)
+                .body("error", equalTo("No order with id sdsdasdasd."));
+
+
+    }
+
 
     public String getOderId() {
         return given()
